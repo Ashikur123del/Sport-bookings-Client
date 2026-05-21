72137afbf7b40775dc9189ddbe8a25eb9b17f43c
ashik <leadskillit@gmail.com>
2026-05-21 11:10:01 +0600

    error issu fiexd

--- src/app/api/auth/[...all]/route.js
+++ src/app/api/auth/[...all]/route.js
@@ -1,7 +1,7 @@
 
 export const dynamic = "force-dynamic";
 
-import { auth } from "@/lib/auth"; // path to your auth file
+import { auth } from "@/lib/auth"; 
 import { toNextJsHandler } from "better-auth/next-js";
 
 export const { POST, GET } = toNextJsHandler(auth);
